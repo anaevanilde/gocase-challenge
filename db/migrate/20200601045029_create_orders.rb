7f29868e1c759f579e9e816b87abc471cfdab299
Eva <anaevanilde@gmail.com>
2020-06-01 02:04:13 -0300

    Create main models order and batch.

---
+++ db/migrate/20200601045029_create_orders.rb
@@ -0,0 +1,16 @@
+class CreateOrders < ActiveRecord::Migration[6.0]
+  def change
+    create_table :orders do |t|
+      t.string :reference
+      t.string :client_name
+      t.string :address
+      t.string :delivery_service
+      t.float :total_value
+      t.string :status
+      t.jsonb :line_items
+      t.references :batch, index: true, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
