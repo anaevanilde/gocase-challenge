421a832258222980ee0d1cdd85a41ad06bdf12a3
Eva <anaevanilde@gmail.com>
2020-06-10 22:32:38 -0300

    Install annotate gem

--- app/models/batch.rb
+++ app/models/batch.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: batches
+#
+#  id               :bigint           not null, primary key
+#  purchase_channel :string
+#  reference        :string
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
 class Batch < ApplicationRecord
   has_many :orders
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,27 @@
+# == Schema Information
+#
+# Table name: orders
+#
+#  id               :bigint           not null, primary key
+#  address          :string
+#  client_name      :string
+#  delivery_service :string
+#  line_items       :jsonb
+#  reference        :string
+#  status           :string
+#  total_value      :float
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#  batch_id         :bigint
+#
+# Indexes
+#
+#  index_orders_on_batch_id  (batch_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (batch_id => batches.id)
+#
 class Order < ApplicationRecord
   belongs_to :batch
 end
